60aef235c716c2dbb2fe425bb76cc652eb116ee5
RobertGee <robertgouveia03@hotmail.com>
2023-03-30 00:09:34 +0100

    Update #01
    Game Manager Created, Game Loop started with level data at basic

---
+++ GmJm3/Assets/Scripts/PlaySessionManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlaySessionManager : MonoBehaviour
+{
+    public static PlaySessionManager Instance;
+    LevelData Tutorial;
+
+    private void Awake()
+    {
+        //makes it so there is always only one instance
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        SceneManager.sceneLoaded += OnSceneLoad;
+        LevelManager.LevelEnd += HandleLevelEnd;
+    }
+
+    void OnSceneLoad(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Tutorial")
+        {
+            //load tutorial
+            LevelManager.StartLevel();
+        }
+    }
+
+    void HandleLevelEnd(bool playerAlive)
+    {
+        if (playerAlive)
+        {
+            //load room according to index (next room in current path)
+        }
+        else
+        {
+            SceneManager.LoadScene("MainMenu");
+            //this will be death screen
+        }
+    }
+}
