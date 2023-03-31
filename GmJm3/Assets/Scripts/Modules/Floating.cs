4832c31e42b74608afaf8baee300c02fc08ca31b
AlanCroda <78557195+AlanCroda@users.noreply.github.com>
2023-03-31 14:35:07 -0600

    Float powerup

--- GmJm3/Assets/Scripts/Modules/Floating.cs
+++ GmJm3/Assets/Scripts/Modules/Floating.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Floating : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Player _player;
+    [SerializeField] bool _isFloating;
+    [SerializeField] float _floatSpeed;
+    private float _noFloatSpeed;
+
+    private void Awake()
     {
-        
+        _player = GetComponent<Player>();
+        _noFloatSpeed = _player.maxFallSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (_player.hasFloatingPowerup)
+        {
+            checkFloat();
+        }
+    }
+
+    public void checkFloat()
+    {
+        // TODO: Research how to use "Jump" InputSystem event as a KeyHeldDown to activate this method,
+        // for now it's bound to Spacebar held down.
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _isFloating = true;
+        }
+        else
+        {
+            _isFloating = false;
+        }
+
+        if (_isFloating && !_player.collisionScript.isGrounded())
+        {
+            _player.maxFallSpeed = _floatSpeed;
+        }
+        else
+        {
+            _player.maxFallSpeed = _noFloatSpeed;
+        }
     }
 }
